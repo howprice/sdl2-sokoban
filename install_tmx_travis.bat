fa69b4de1b4966b74d5fdf3d5e7bad88218d6834
howprice <howprice@users.noreply.github.com>
2019-07-28 14:58:54 +0100

    Travis: Add 64-bit builds

--- install_tmx_travis.bat
+++ install_tmx_travis.bat
@@ -3,4 +3,7 @@ REM @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
 call install_tmx.bat
 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+call install_tmx.bat
+
 EXIT /B 0
