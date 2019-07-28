3804cf00aa7f09405f5059ba01e5ce0fe1962cbb
howprice <howprice@users.noreply.github.com>
2019-07-28 12:23:39 +0100

    Fix install_sdl.bat to work from Git Bash shell
    Windows tar can unzip .zip files, but Linux version cannot.

--- install_sdl.bat
+++ install_sdl.bat
@@ -1,4 +1,4 @@
-@echo off
+REM @echo off
 
 SET SDL2_VERSION=2.0.10
 SET SDL2_IMAGE_VERSION=2.0.5
@@ -8,13 +8,15 @@ IF NOT EXIST tmp mkdir tmp
 pushd tmp
 
 
-REM SDL
+REM SDL2
 SET SDL2_ZIP=SDL2-devel-%SDL2_VERSION%-VC.zip
 IF NOT EXIST %SDL2_ZIP% (
 	ECHO Downloading %SDL2_ZIP%
 	curl -O https://www.libsdl.org/release/%SDL2_ZIP%
 )
 tar xfz %SDL2_ZIP%
+IF %ERRORLEVEL% NEQ 0 unzip %SDL2_ZIP%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 0
 robocopy /MOVE /S /E SDL2-%SDL2_VERSION% ..\3rdParty\SDL2
 
 
@@ -25,6 +27,8 @@ IF NOT EXIST %SDL2_IMAGE_ZIP% (
 	curl -O https://www.libsdl.org/projects/SDL_image/release/%SDL2_IMAGE_ZIP%
 )
 tar xfz %SDL2_IMAGE_ZIP%
+IF %ERRORLEVEL% NEQ 0 unzip %SDL2_IMAGE_ZIP%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 0
 robocopy /MOVE /S /E SDL2_image-%SDL2_IMAGE_VERSION% ..\3rdParty\SDL2_image
 
 
@@ -35,7 +39,8 @@ IF NOT EXIST %SDL2_TTF_ZIP% (
 	curl -O https://www.libsdl.org/projects/SDL_ttf/release/%SDL2_TTF_ZIP%
 )
 tar xfz %SDL2_TTF_ZIP%
+IF %ERRORLEVEL% NEQ 0 unzip %SDL2_TTF_ZIP%
+IF %ERRORLEVEL% NEQ 0 EXIT /B 0
 robocopy /MOVE /S /E SDL2_ttf-%SDL2_TTF_VERSION% ..\3rdParty\SDL2_ttf
-
 
 popd
