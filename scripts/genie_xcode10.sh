29a301f37b56ed5f8002d39eae59074a28607bef
howprice <howprice@users.noreply.github.com>
2019-07-30 19:21:13 +0100

    Make genie_xcode10.sh pwd independent

--- scripts/genie_xcode10.sh
+++ scripts/genie_xcode10.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-./tools/bin/macosx/genie xcode10
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd $SCRIPT_DIR > /dev/null
+cd ..  # up into repo root
+
+tools/bin/macosx/genie xcode10
+
+popd > /dev/null
