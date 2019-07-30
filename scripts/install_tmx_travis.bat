9eeb279493993cfeda10f00c462b51255cd2cc50
howprice <howprice@users.noreply.github.com>
2019-07-30 18:31:09 +0100

    Update install_tmx_travis.bat

--- scripts/install_tmx_travis.bat
+++ scripts/install_tmx_travis.bat
@@ -2,12 +2,13 @@
 
 SET script_path=%~dp0
 pushd %script_path%
+cd ..
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
-call install_tmx.bat
+call scripts\install_tmx.bat
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-call install_tmx.bat
+call scripts\install_tmx.bat
 
 popd
 
